14d5389ea10288013e711350fbb5c4e5f0f531d8
Jarani1 <shahin.salehi1@outlook.com>
2017-10-25 22:20:15 +0200

    Update client.java

--- client.java
+++ client.java
@@ -23,6 +23,7 @@ public static ArrayList<String> commands = new ArrayList<String>(); //From serve
 static Thread tcpListen;
 ArrayList<sprites> players = new ArrayList<sprites>(); //add player dynamically
 
+
 public static class listen extends Thread
 {
 	public void run()
@@ -33,28 +34,23 @@ public static class listen extends Thread
 			socket = new Socket("192.168.1.3",PORT);
 			System.out.printf("Connected to HOST IP: %s and PORT: %s\n",socket.getInetAddress(),socket.getPort());
 			InputStreamReader in = new InputStreamReader(socket.getInputStream());
+			BufferedInputStream buffin = new BufferedInputStream(socket.getInputStream());
+			
 			
 			//send ip and port
 			
 			OutputStreamWriter out1 = new OutputStreamWriter(socket.getOutputStream());
 			char[] writeBuffer = new char[256];
-
-			String port = Integer.toString(socket.getLocalPort());
-			String send = "INIT:"+socket.getLocalAddress().getHostAddress()+":"+port;
-			System.out.println(send);
-			writeBuffer = send.toCharArray();
-			out1.write(send);
-			out1.flush();
-			
-			
 			
 			char[] readBuffer = new char[256];
 
 			int lastindx=0;
 			
+		
+			
 			while(in.read(readBuffer)!=-1)
 			{
-
+				
 				//basically cut message down from 256 to actual message
 				for(int i = 0; i<256; i++)
 				{
@@ -64,15 +60,35 @@ public static class listen extends Thread
 						break;
 					}
 				}
+
 				
 				//make string out of message
-				String bufferRead = new String(readBuffer,0,lastindx);
+				String bufferRead = new String(readBuffer,0,8);
 				//add to commandArray
 				commands.add(bufferRead);
 				System.out.printf("server says: ");
 				System.out.println(bufferRead);
-				Arrays.fill(readBuffer, (char)0); //clear 
 				
+
+				if(bufferRead.equals("malin"))
+				{
+					System.out.println("YEHA BABY WE GOT IT");
+				}
+				
+				//Sending out ID number to our server thread
+				String clientid = "ID:";
+				String check[] = bufferRead.split(":");
+				
+				if(check[0].equals("clntid"))
+				{
+					clientid += check[1];
+					System.out.printf("Our ID num: %s\n", clientid);					
+					writeBuffer = clientid.toCharArray();
+					out1.write(clientid);
+					out1.flush();
+				}
+				Arrays.fill(readBuffer, (char)0); //clear 
+				System.out.println("cleared");
 			}
 			
 		} catch(IOException e) {
@@ -104,9 +120,22 @@ public static class listen extends Thread
 		//Set player
 		sprites player = new sprites();
 		player.setImage("yrthuvud.png");
-		player.setPostition(Math.random()*1280, Math.random()*720);
+		player.setPostition(640, 360);
 		player.setVelocity(0, 0);
 		
+		//init playerlist
+		sprites temp = new sprites();
+		//just to fill the playes, these will get moved as players join
+		players.add(temp);
+		players.add(temp);
+		players.add(temp);
+		players.add(temp);
+		players.add(temp);
+		players.add(temp);
+		players.add(temp);
+		players.add(temp);
+		players.add(temp);
+		players.add(temp);
 		
 		
 		
@@ -128,7 +157,95 @@ public static class listen extends Thread
 					e.printStackTrace();
 				}
 				
-				//need an if that creates new sprite, possible???
+
+				//HANDLE JOIN
+				
+				for(int i=0;i<commands.size();i++)
+				{
+					if(commands.get(i).contains("JOINSE"))
+					{
+						//Dynamically add the player to the game
+						//Set client in the array with ID 
+						
+						System.out.println("got join command indx");
+						String [] crID = commands.get(i).split(":");
+						System.out.println(Arrays.toString(crID));
+						int cID = Integer.parseInt(crID[1]);
+						sprites client = new sprites();
+						players.remove(cID);
+						players.add(cID, client);
+						client.setImage("yrthuvud.png");
+						client.setPostition(640, 360);
+						client.setVelocity(0, 0);
+						commands.remove(i);
+						break;
+						
+					}
+				}
+				//So it doesnt add 4ever
+				for(sprites spr : players)
+				{
+					spr.setVelocity(0, 0);
+				}
+				//other player right
+				for(int i=0;i<commands.size();i++)
+				{
+					if(commands.get(i).contains("CRIGHT"))
+					{
+						System.out.println("I entered cright");
+						
+						String [] crID = commands.get(i).split(":");
+						System.out.println(Arrays.toString(crID));	
+						int cID = Integer.parseInt(crID[1]);
+						players.get(cID).addVelocity(3.5, 0);
+						commands.remove(i);
+						break;
+					}
+				}
+				
+				//other player left
+				for(int i=0;i<commands.size();i++)
+				{
+					if(commands.get(i).contains("CLEFTT"))
+					{
+						
+						String [] crID = commands.get(i).split(":");
+						int cID = Integer.parseInt(crID[1]);
+						players.get(cID).addVelocity(-3.5, 0);
+						commands.remove(i);
+						break;
+					}
+				}
+				
+				//other player UP
+				for(int i=0;i<commands.size();i++)
+				{
+					if(commands.get(i).contains("CUUPPP"))
+					{
+						
+						String [] crID = commands.get(i).split(":");
+						int cID = Integer.parseInt(crID[1]);
+						players.get(cID).addVelocity(0,-3.5);
+						commands.remove(i);
+						break;
+					}
+				}
+				
+				//other player DOWN
+				for(int i=0;i<commands.size();i++)
+				{
+					if(commands.get(i).contains("CDOWNN"))
+					{
+						
+						String [] crID = commands.get(i).split(":");
+						int cID = Integer.parseInt(crID[1]);
+						players.get(cID).addVelocity(0, 3.5);
+						commands.remove(i);
+						break;
+					}
+				}
+				
+				
 				
 				//So it doesnt add forever set 0,0
 				player.setVelocity(0, 0);
@@ -145,12 +262,13 @@ public static class listen extends Thread
 					{
 						e.printStackTrace();
 					}
+					player.addVelocity(-3.5, 0);
 					//NEED IF STATEMENT HERE	
-					if(commands.contains("LEFTOK"))
-					{
-						player.addVelocity(-3.5, 0);
-					}commands.remove("LEFTOK");
-					
+//					if(commands.contains("LEFTOK"))
+//					{
+//						player.addVelocity(-3.5, 0);
+//					}commands.remove("LEFTOK");
+//					
 				}
 				if(input.contains("RIGHT"))
 				{
@@ -164,11 +282,12 @@ public static class listen extends Thread
 					{
 						e.printStackTrace();
 					}
+					player.addVelocity(3.5, 0);
 					//IF STATEMENT HERE
-					if(commands.contains("RIGHTOK"))
-					{
-						player.addVelocity(3.5, 0);
-					}commands.remove("RIGHTOK");
+//					if(commands.contains("RIGHTOK"))
+//					{
+//						player.addVelocity(3.5, 0);
+//					}commands.remove("RIGHTOK");
 					
 				}
 				if(input.contains("UP"))
@@ -183,12 +302,13 @@ public static class listen extends Thread
 					{
 						e.printStackTrace();
 					}
+					player.addVelocity(0, -3.5);
 					//IF STATEMENT HERE
-					if(commands.contains("UPOK"))
-					{
-						player.addVelocity(0, -3.5);
-					}commands.remove("UPOK");
-					
+//					if(commands.contains("UPOK"))
+//					{
+//						
+//					}commands.remove("UPOK");
+//					
 				}
 				if(input.contains("DOWN"))
 				{
@@ -202,12 +322,19 @@ public static class listen extends Thread
 					{
 						e.printStackTrace();
 					}
-					//IF STATEMENT HERE	
-					if(commands.contains("DOWNOK"))
-					{
-						player.addVelocity(0,3.5);
-					}commands.remove("DOWNOK");
+					player.addVelocity(0,3.5);
 					
+					//IF STATEMENT HERE	
+//					if(commands.contains("DOWNOK"))
+//					{
+//						
+//					}commands.remove("DOWNOK");
+//					
+				}
+				
+				for(sprites p : players)
+				{
+					p.update();
 				}
 				
 				if(player.update()==1)
@@ -219,7 +346,12 @@ public static class listen extends Thread
 				gc.clearRect(0, 0, 1280, 720);
 				player.render(gc);
 				
-				
+				//render all players
+				for(sprites pp : players)
+				{
+					pp.render(gc);
+		
+				}
 				
 				
 			}
